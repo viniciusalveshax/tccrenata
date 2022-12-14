4b2bf4d2f1e20052e2b0f66f52afff4d4df012ec
renatarodriguesp <renata.rrp.rrp@gmail.com>
2022-12-14 16:48:59 -0300

    Commit inicial

---
+++ src/perceptiontest/Teste.java
@@ -0,0 +1,24 @@
+package perceptiontest;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author renatarodriguespintos
+ */
+public class Teste {
+   private String instrucaogeral;
+
+    public String getInstrucaogeral() {
+        return instrucaogeral;
+    }
+
+    public void setInstrucaogeral(String instrucaogeral) {
+        this.instrucaogeral = instrucaogeral;
+    }
+   
+}
+
